ab3c6ff1db8e31b570c74b07851b06aaa59de821
joe90joe40 <joe90joe40@hotmail.com>
2015-05-04 18:43:14 +0100

    delete redundant files

--- development/main-app/public/main-client-app/js/index.js
+++
@@ -1,9 +0,0 @@
-var config = require('../../client-config/config');
-var $ = require('../../bower_components/jquery/dist/jquery');
-var myapp = require('./main');
-
-console.log(config);
-$(function(){
-    var Console = new myapp();
-});
-
